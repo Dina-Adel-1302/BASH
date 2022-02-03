57d63f6fb8d83ec34e6a625356fc6840970163f6
Dina-Adel-1302 <97478336+Dina-Adel-1302@users.noreply.github.com>
2022-02-03 12:12:39 +0200

    Delete sort_integers.sh

--- sort_integers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#This script sorts argument integers from high to low
-
-echo "Please enter integers to sort, separated by spaces: "
-read int1 
-
-echo $int1 | tr " " "\n" | sort -n -r 
-
