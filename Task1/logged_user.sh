0c4548fd618e6c397a90c8bfd9acfd756ca03886
Dina-Adel-1302 <97478336+Dina-Adel-1302@users.noreply.github.com>
2022-02-03 12:08:49 +0200

    Add files via upload

---
+++ Task1/logged_user.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+#This script prints the loggen in user. 
+
+echo "The logged in user is: $USER"
---
+++ Task1/myadd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#This script takes a number from the user and add 1 to it.
+
+echo "Please enter a number: "
+read num
+
+echo "$num + 1 = $(($num+1))"
