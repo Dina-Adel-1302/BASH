1532671c102fabeac6a890a4511a2c85970f6cc9
Dina-Adel-1302 <97478336+Dina-Adel-1302@users.noreply.github.com>
2022-02-07 09:54:38 +0200

    Add files via upload

--- Task2/q4.sh
+++ Task2/q4.sh
@@ -6,11 +6,36 @@
 #	-c: print the number of matched given word.
 #	-d: print the file after deleting the lines that contain the given word.
 
-case $1 in
-	(-i) grep $2 $3;;
-	(-c) grep -o $2 $3 | wc -w ;;
-	(-d) grep -v $2 $3;;
-	(*) echo undefined arguments;;
+echo "Please enter 3 arguments: option[-i,-c, -d], word, file:"
+echo "-i: print the lines that contain the given word."
+echo "-c: print the number of matched given word."
+echo "-d: print the file after deleting the lines that contain the given word."
+echo "Note: this script will not delete any data.It is just processing temporary data."
+echo Enter your option: 
+read option
+echo Enter a word:
+read word
+echo Enter a file:
+read file
+
+if !(test -f $file)
+then 
+	echo "file not found"
+	exit 1
+fi
+
+case $option in
+	(-i) if !(grep $word $file); then
+		echo "word not found"
+		fi
+		;;
+	(-c) if [ $(grep -o $word $file | wc -w) -eq 0 ]; then
+		echo "word not found"
+		fi       
+		;;
+	(-d) grep -v $word $file #print all the lines that does not contain the word - reverted search
+		;;
+	(*) echo invalid option;;
 esac
 
 
